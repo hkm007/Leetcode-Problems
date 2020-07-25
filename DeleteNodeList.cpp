ed77f828f026d531dc23f3187843c41d6e8ccc59
hkm007 <hkm0072001@gmail.com>
2020-07-25 23:55:54 +0530

    init

---
+++ DeleteNodeList.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+        delete(temp);
+    }
+};
