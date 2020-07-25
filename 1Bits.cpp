57ec3d6d3c4b561e8936d2d94a64e09cec0005da
hkm007 <hkm0072001@gmail.com>
2020-07-26 00:18:55 +0530

    init

---
+++ 1Bits.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        
+        while(n) {
+            if(n&1 == 1) res++;
+            n >>= 1;
+        }
+        
+        return res;
+    }
+};
